4e1934edb888e1f4fbeb1c67a792b41f5f0ebaa3
SayulRamirez <sayulramirez@gmail.com>
2024-08-29 21:40:31 -0600

    Agregado el componente para mostrar las tareas del usuario

---
+++ src/components/MeTasks.jsx
@@ -0,0 +1,14 @@
+export const MeTasks = () => {
+    return (
+        <div>
+            <div>
+                <p>any title</p>
+                <p>any priority</p>
+            </div>
+            <div>
+                <p>any title</p>
+                <p>any priority</p>
+            </div>
+        </div>
+    );
+}

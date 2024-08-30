a731494045e4c2a6448d9491b6d7facc68cf3fa2
SayulRamirez <sayulramirez@gmail.com>
2024-08-30 00:04:20 -0600

    Agregado el BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ import {Register} from "./components/Register.jsx";
 import {Main} from "./components/Main.jsx";
 import {NewProject} from "./components/NewProject.jsx";
 import {Project} from "./components/Project.jsx";
+import {Task} from "./components/Task.jsx";
+import {TasksDetails} from "./components/TasksDetails.jsx";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
   //const [count, setCount] = useState(0)
@@ -14,11 +17,23 @@ function App() {
   return (
     <>
       <div className="card">
-          <NewProject newProject/>
-          <Login login/>
-          <Register register/>
-          <Main main/>
-          <Project project/>
+          <BrowserRouter>
+              header
+              <div>
+                  <Routes>
+                      <Route path="/" element={<Login/>} />
+                      <Route path="/login" element={<Login/>} />
+                      <Route path="/register" element={<Register/>} />
+                  </Routes>
+              </div>
+              <Register register/>
+              <NewProject newProject/>
+              <Login login/>
+              <Main main/>
+              <Project project/>
+              <Task task/>
+              <TasksDetails taskDetails/>
+          </BrowserRouter>
       </div>
     </>
   )

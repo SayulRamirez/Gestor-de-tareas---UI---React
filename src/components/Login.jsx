31678b1bb5fbc21651eab54271085170d2a9e58b
SayulRamirez <sayulramirez@gmail.com>
2024-08-30 22:02:41 -0600

    Agregado la logica para el manejo del login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,31 +1,50 @@
 import {useState} from "react";
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
+import Auth from "../services/Auth.js";
 
 export const Login = () => {
 
     const [email, setEmail] = useState('');
-    const[password, setPassword] = useState('');
+    const [password, setPassword] = useState('');
 
-    function showCredentials() {
-        alert(email + ' ' + password);
+    const navigate = useNavigate();
+
+    function login(event) {
+
+        event.preventDefault();
+
+        const request = {
+            email: email,
+            password: password
+        }
+
+        Auth.login(request).then(response => {
+            if (response.status === 200) {
+                localStorage.setItem("token", response.data.token);
+                navigate("/reception");
+            }
+        }).catch(error => {
+            console.error("Error en la solicitud:", error.response ? error.response.data : error.message)
+        })
     }
 
     return(
-        <>
-            <div className="card">
-                <h1>Login</h1>
-                <form>
-                    <p>Usuario: <input type='email'
-                                       name='email'
-                                       value={email} onChange={(e) => setEmail(e.target.value)} />
-                    </p>
-                    <p>Contraseña: <input type='text'
-                                          name='password'
-                                          value={password} onChange={(e) => setPassword(e.target.value)}/></p>
-                    <button onClick={showCredentials}>Ingresar</button>
-                </form>
-                <p>¿Aun no tienes cuenta? <Link to='/register'>da click aquí</Link></p>
-            </div>
-        </>
+        <div className="card">
+            <h1>Login</h1>
+            <form onSubmit={login}>
+                <p>Usuario: <input type='email'
+                                   name='email'
+                                   value={email}
+                                   onChange={(e) => setEmail(e.target.value)} />
+                </p>
+                <p>Contraseña: <input type='password'
+                                      name='password'
+                                      value={password}
+                                      onChange={(e) => setPassword(e.target.value)}/>
+                </p>
+                <button type='submit'>Ingresar</button>
+            </form>
+            <p>¿Aun no tienes cuenta? <Link to='/register'>da click aquí</Link></p>
+        </div>
     );
 }

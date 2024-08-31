0b3124092f71369b3524f2eea0d789080a8be4cc
SayulRamirez <sayulramirez@gmail.com>
2024-08-31 11:27:59 -0600

    Mostrando los proyectos del usuario

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,19 @@
+import {useEffect, useState} from "react";
+import Project from "../services/Project.js";
+import {Link} from "react-router-dom";
+
 export const Main = () => {
+
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        Project.getAllProjects().then(response => {
+            setProjects(response.data);
+        }).catch(error => {
+            console.error("Error fetching projects:", error);
+        });
+    }, []);
+
     return (
         <div>
             <div>
@@ -6,14 +21,12 @@ export const Main = () => {
                 <button>Mis proyectos</button>
                 <button>Mis tareas</button>
             </div>
-            <div>
-                <p>Cualquier titulo</p>
-                <p>Cualquier status</p>
-            </div>
-            <div>
-                <p>Cualquier titulo</p>
-                <p>Cualquier status</p>
-            </div>
+            {projects.map(project => (
+                    <Link to={`/project/${project.id}`} key={project.id}>
+                        <p>Título: {project.title}</p>
+                        <p>Estatus: {project.status}</p>
+                    </Link>
+            ))}
         </div>
     );
 }

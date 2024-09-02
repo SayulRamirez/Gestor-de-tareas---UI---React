6e806c1cff15d6ccf5fbeea28bcc15291ec1c4a2
SayulRamirez <sayulramirez@gmail.com>
2024-09-01 22:58:30 -0600

    Agregadas las rutas a los diferentes componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-//import { useState } from 'react'
-
 import './App.css'
 import {Login} from "./components/Login.jsx";
 import {Register} from "./components/Register.jsx";
@@ -9,6 +7,9 @@ import {Project} from "./components/Project.jsx";
 import {Task} from "./components/Task.jsx";
 import {TasksDetails} from "./components/TasksDetails.jsx";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {NewTask} from "./components/NewTask.jsx";
+import {Report} from "./components/Report.jsx";
+import {Tasks} from "./components/Tasks.jsx";
 
 function App() {
 
@@ -16,19 +17,23 @@ function App() {
     <>
       <div className="card">
           <BrowserRouter>
-              header
               <div>
                   <Routes>
                       <Route path="/" element={<Login/>} />
                       <Route path="/login" element={<Login/>} />
                       <Route path="/register" element={<Register/>} />
                       <Route path="/reception" element={<Main/>} />
+
+                      <Route path="/project/:id" element={<Project/>} />
+                      <Route path="/report/:idProject" element={<Report/>} />
+                      <Route path="/project/new" element={<NewProject/>} />
+
+                      <Route path="/tasks/:idProject" element={<NewTask/>} />
+                      <Route path="/tasks/details/:idUser/:idProject" element={<TasksDetails/>} />
+                      <Route path="/tasks/all/:idUser" element={<Tasks/>} />
+                      <Route path="/task/:id" element={<Task/>} />
                   </Routes>
               </div>
-              <NewProject newProject/>
-              <Project project/>
-              <Task task/>
-              <TasksDetails taskDetails/>
           </BrowserRouter>
       </div>
     </>

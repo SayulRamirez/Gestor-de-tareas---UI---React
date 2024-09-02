b4af39b83b64a98137e7164cb1f334db73b35f0f
SayulRamirez <sayulramirez@gmail.com>
2024-09-01 22:57:15 -0600

    Agregado los Links para mostrar mis proyectos y Mis tareas

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,16 @@
 import {useEffect, useState} from "react";
-import Project from "../services/Project.js";
+import ProjectService from "../services/Project.js";
 import {Link} from "react-router-dom";
+import {getIdFromToken} from "../utils/getIdFromToken.js";
 
 export const Main = () => {
 
     const [projects, setProjects] = useState([]);
 
+    const id = getIdFromToken();
+
     useEffect(() => {
-        Project.getAllProjects().then(response => {
+        ProjectService.getAllProjects().then(response => {
             setProjects(response.data);
         }).catch(error => {
             console.error("Error fetching projects:", error);
@@ -17,9 +20,9 @@ export const Main = () => {
     return (
         <div>
             <div>
-                <button>Nuevo proyecto</button>
-                <button>Mis proyectos</button>
-                <button>Mis tareas</button>
+                <Link to={`/project/new`}>Nuevo proyecto</Link>
+                <Link to={`/reception`}>Mis proyectos</Link>
+                <Link to={`/tasks/all/${id}`}>Mis tareas</Link>
             </div>
             {projects.map(project => (
                     <Link to={`/project/${project.id}`} key={project.id}>

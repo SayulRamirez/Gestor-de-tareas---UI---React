0f43583ebfa8a3859980b4ba391b524161e1065b
SayulRamirez <sayulramirez@gmail.com>
2024-09-01 22:55:01 -0600

    Agregada la funcionalidad para ver los detalles de una tarea y poder actualizarla

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,30 +1,57 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import TaskService from "../services/TaskService.js";
+import {Link, useParams} from "react-router-dom";
+import {getIdFromToken} from "../utils/getIdFromToken.js";
 
 export const Task = () => {
 
-    const [status, setStatus] = useState('')
+    const [task, setTask] = useState({});
+    const [status, setStatus] = useState('');
+    const {id} = useParams();
+    const idUser = getIdFromToken();
 
-    function showData() {
-        alert(status);
+    useEffect(() => {
+
+        TaskService.getTask(id).then(response => {
+            setTask(response.data);
+            setStatus(response.data.status);
+            
+        }).catch(error => {
+            console.error("Error fetching tasks:", error);
+        })
+    }, [id]);
+
+    const updateStatus = (id) => {
+
+        const request = {
+            id: id,
+            status: status
+        }
+
+        TaskService.update(request).then(response => {
+            setStatus(response.data);
+        }).catch(error => {
+            console.error("Error fetching tasks:", error);
+        })
     }
 
-    return (
-        <div>
-            <p>Any title</p>
-            <p>Any description</p>
-            <p>Any status:
-                <select onChange={e => setStatus(e.target.value)}>
-                    <option value={status}/>
-                    <option value='en progreso'/>
-                    <option value='pendiente'/>
-                    <option value='completa'/>
+    return (         
+        <div key={task.id}>
+            <Link to={`/tasks/all/${idUser}`}>Regresar</Link>
+            <p>Titulo: {task.title}</p>
+            <p>Descripción: {task.description}</p>
+            <p>Estatus:
+                <select value={status} onChange={e => setStatus(e.target.value)}>
+                    <option value='IN_PROGRESS'>Pendiente</option>
+                    <option value='PENDING'>En progreso</option>
+                    <option value='COMPLETE'>Completa</option>
                 </select>
             </p>
-             <p>Any date</p>
-            <p>Any priority</p>
-            <p>Any date create</p>
-            <p>Any runtime</p>
-            <button onClick={showData}/>
+            <p>Entrega estimada: {task.estimate_completion}</p>
+            <p>Prioridad: {task.priority}</p>
+            <p>Fecha de asignación: {task.create_date}</p>
+            <p>Tiempo de ejecución: {task.runtime}</p>
+            <button onClick={() => updateStatus(task.id)}>Actualizar estatus</button>
         </div>
     );
 }

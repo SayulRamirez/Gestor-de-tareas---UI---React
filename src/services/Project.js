a39afcb33ac61f412fe1cfb3439666e27c604102
SayulRamirez <sayulramirez@gmail.com>
2024-09-01 21:14:07 -0600

    Agregado los metodos getReport, updateStatus, deletePro y createProject

--- src/services/Project.js
+++ src/services/Project.js
@@ -3,14 +3,34 @@ import {getIdFromToken} from "../utils/getIdFromToken.js";
 
 const URL = "/projects"
 
-class Project {
+class ProjectService {
 
     getAllProjects() {
 
         const id = getIdFromToken();
 
-        return apiClient(`${URL}/all/${id}`);
+        return apiClient.get(`${URL}/all/${id}`);
+    }
+
+    getProject(id) {
+        return apiClient.get(`${URL}/${id}`);
+    }
+
+    getReport(id) {
+        return apiClient.get(`${URL}/report/${id}`);
+    }
+
+    updateStatus(request) {
+        return apiClient.put(`${URL}`, request);
+    }
+
+    deleteProject(id) {
+        return apiClient.delete(`${URL}/${id}`);
+    }
+
+    createProject(request) {
+        return apiClient.post(`${URL}`, request);
     }
 }
 
-export default new Project();
+export default new ProjectService();

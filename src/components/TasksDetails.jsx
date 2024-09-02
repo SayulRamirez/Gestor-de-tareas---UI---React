317d8857528283326b0d38e7b08a39353d9c1080
SayulRamirez <sayulramirez@gmail.com>
2024-09-01 21:15:20 -0600

    Agregada la funcionalidad al componente TasksDetails

--- src/components/TasksDetails.jsx
+++ src/components/TasksDetails.jsx
@@ -1,19 +1,50 @@
+import {useEffect, useState} from "react";
+import TaskService from "../services/TaskService.js";
+import {Link, useParams} from "react-router-dom";
+
 export const TasksDetails = () => {
 
-    function showData() {
-        alert('showing data');
+    const  [tasks, setTasks] = useState([]);
+    const {idUser, idProject} = useParams();
+
+    useEffect(() => {
+        TaskService.getTasksFromProject(idUser, idProject).then(response => {
+            setTasks(response.data);
+        }).catch(error => {
+            console.error("Fetch error: ", error);
+        })
+    }, [idProject, idUser])
+
+    const deleteTask = (id) => {
+        TaskService.deleteTask(id).then(response => {
+            if (response.data.status === 404) {
+                setTasks(prevTasks => prevTasks.filter(task => task.id !== id));
+            }
+        }).catch(error => {
+            if (error.response && error.response.status === 404) {
+                console.error("Task not found: ", error);
+                setTasks(prevTasks => prevTasks.filter(task => task.id !== id));
+            } else {
+                console.error("Fetch error: ", error);
+            }
+        });
     }
 
     return (
         <div>
-            <p>Any title</p>
-            <p>Any description</p>
-            <p>Any status:</p>
-            <p>Any date</p>
-            <p>Any priority</p>
-            <p>Any date create</p>
-            <p>Any runtime</p>
-            <button onClick={showData}/>
+            <Link to={`/report/${idProject}`}>Regresar al reporte</Link>
+            {tasks.map(task => (
+                <div key={task.id}>
+                    <p>Titulo: {task.title}</p>
+                    <p>Descripción: {task.description}</p>
+                    <p>Estatus: {task.status}</p>
+                    <p>Fecha de entrega: {task.estimate_delivery}</p>
+                    <p>Prioridad: {task.priority}</p>
+                    <p>Fecha de creación: {task.create_date}</p>
+                    <p>Días de ejecución: {task.runtime}</p>
+                    <button onClick={() => deleteTask(task.id)}>Eliminar tarea</button>
+                </div>
+            ))}
         </div>
     );
 };
